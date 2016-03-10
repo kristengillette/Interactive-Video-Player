4360bfc7fec4236688484adffa9a8c24a056a79f
Kristen Gillette <kristenmgillette@gmail.com>
2016-03-10 16:10:42 -0500

    Implementing Highlighting

---
+++ highlight.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+	var times = [0, 4.13, 7.535, 11.27, 13.96, 17.94, 22.37, 26.88, 30.92, 34.73, 39.43, 41.19, 42.35, 46.3, 49.27, 53.76, 57.78, 60.15];
+	var time;
+
+$('video')[0].player.media.addEventListener('timeupdate', function() {
+	 time = $('video')[0].player.media.currentTime;
+	 console.log(time);
+}); 
+
+$('video')[0].player.media.addEventListener('timeupdate', function() {
+	 for (var i = 0; i < times.length; i++) {
+	 	if (time > times[i] && time < times[i+1]) {
+	 		$('#' + i).css('color', 'orange');
+	 	} else if (time == times[i]) {
+	 		console.log(i);
+	 	}
+	 	else {
+	 		$('#' + i).css('color', '#384047');
+	 	}
+	 };
+}); 
+	
+});
